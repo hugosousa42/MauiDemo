0c4a0ead4f07d0c0580c0f566713d19dcdeb021b
hugosousa42 <hugosousa42@proton.me>
2024-12-01 05:28:41 +0000

    XAML language e Pages.

--- MauiDemo/App.xaml.cs
+++ MauiDemo/App.xaml.cs
@@ -6,7 +6,11 @@
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            var navPage = new NavigationPage(new MinhaPagina());
+            navPage.BarBackground = Colors.Yellow;
+            navPage.BarTextColor = Colors.Blue;
+
+            MainPage = navPage;
         }
     }
 }
